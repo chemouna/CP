e4aa99e9e4ca9207ce466d838f5b4ceb75752478
Mouna Cheikhna <cheikhnamouna@gmail.com>
2018-07-04 22:23:47 +0100

    Continue setup

---
+++ app/src/main/java/com/mounacheikhna/cp/CPApplication.kt
@@ -0,0 +1,12 @@
+package com.mounacheikhna.cp
+
+import android.app.Application
+
+class CPApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+
+}
---
+++ codeforces/src/main/java/com/mounacheikhna/codeforces/CodeforcesModule.kt
@@ -0,0 +1,8 @@
+package com.mounacheikhna.codeforces
+
+/**
+ * Created by mounacheikhna on 04/07/2018.
+ */
+//@Module
+class CodeforcesModule {
+}
