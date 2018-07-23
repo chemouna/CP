1a65e2346af67492d02b69f08e5151ff0a50d002
Mouna Cheikhna <cheikhnamouna@gmail.com>
2018-07-23 07:30:05 +0100

    Fix build issue

--- codeforces/src/main/java/com/mounacheikhna/codeforces/CodeforcesModule.kt
+++ codeforces/src/main/java/com/mounacheikhna/codeforces/CodeforcesModule.kt
@@ -5,4 +5,27 @@ package com.mounacheikhna.codeforces
  */
 //@Module
 class CodeforcesModule {
+
+    /*@Provides
+    @Singleton
+    fun documentationService(baseUrl: String): CodeforcesService {
+        // TODO logging
+        //HttpLoggingInterceptor.Logger logger = message -> Timber.tag("HTTP").d(message);
+
+        val client = OkHttpClient.Builder() //
+                //.addNetworkInterceptor(new HttpLoggingInterceptor(logger).setLevel(BASIC)) //
+                .build()
+
+        val contentType = requireNonNull(MediaType.parse("application/json; charset=utf-8"))
+        val json = JSON.Companion.getUnquoted()
+        val retrofit = Retrofit.Builder() //
+                .baseUrl(baseUrl.getUrl()) //
+                .client(client) //
+                .addConverterFactory(
+                        KotlinSerializationConverterFactory.stringBased(contentType, ???({ json.parse() }), ???({ json.stringify() }))) //
+        .addCallAdapterFactory(CoroutineCallAdapterFactory.create()) //
+                .build()
+
+        return retrofit.create(DocumentationService::class.java)*/
+    }
 }
---
+++ codeforces/src/main/java/com/mounacheikhna/codeforces/CodeforcesService.kt
@@ -0,0 +1,6 @@
+package com.mounacheikhna.codeforces
+
+interface CodeforcesService {
+
+    // @GET
+}
